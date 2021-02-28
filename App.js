accbe7779145e2280a0d10b373cdeb2b28a00355
AlejandraCeniceros <dcac@outlook.es>
2021-02-27 17:52:20 -0700

    first updates

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity, Alert } from 'react-native';
+import image from './assets/LogoPongui.png'
 
-export default function App() {
+const App =() => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style ={styles.container}>
+      <Text style ={styles.title}>Clinica de Fisioterapia</Text>
+      <Image 
+      source={image}
+      style ={styles.image}
+      />
+      <TouchableOpacity
+        onPress={() => Alert.alert('Hello"""')}
+        style={styles.btn}
+      >
+        <Text style={styles.btnText }>Iniciar sesión : Fisioterapeuta</Text>
+      </TouchableOpacity>
+      
+
+      <Text style={styles.text }> ó</Text>
+
+      <TouchableOpacity
+        onPress={() => Alert.alert('Hello"""')}
+        style={styles.btn}
+      >
+        <Text style={styles.btnText }>Iniciar sesión : Biomédico</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
+const styles = StyleSheet.create ({
+  container: { 
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent : "center",
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#292929",
   },
+  title : {fontSize: 25, color: "#fff"},
+  image: {height : 150, width: 150, borderRadius:90},
+  btn: {
+    backgroundColor: "#228b22",
+    padding: 7,
+    marginTop :50,
+  },
+  btnText:{
+    color: "#fff",
+    fontSize: 20,
+  },
+  text : {fontSize: 25, color: "#fff", marginTop:20},
+
+
+
 });
+
+export default App;
